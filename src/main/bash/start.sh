48746528bb96b6637b1303a5c36a8f68d7be42da
Denis Tsyplakov <denis.tsyplakov@gmail.com>
2019-04-07 21:49:44 +0300

    comments on start.sh

--- src/main/bash/start.sh
+++ src/main/bash/start.sh
@@ -2,4 +2,6 @@
 if [[ -f /config/application.properties ]]; then
    cp /config/application.properties /application.properties
 fi
+
+# In real world here we may want to detach java from bash, but for simplicity reasons let's keep it simple
 java -jar /app.jar
